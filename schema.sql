5e9e7b37df94361b197b36d605852f28f27cbb29
Ramitsan <ramitsan@gmail.com>
2022-03-04 00:52:37 +0300

    Исправляет схему базы данных

--- schema.sql
+++ schema.sql
@@ -6,75 +6,82 @@ USE readme;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  user_email VARCHAR(128) NOT NULL UNIQUE,
-  user_login VARCHAR(128) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL,
-  user_avatar VARCHAR(255)
+  registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(128) NOT NULL UNIQUE,
+  login VARCHAR(128) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS posts (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  post_title VARCHAR(128) NOT NULL,
-  post_text_content VARCHAR(255) NULL,
-  post_quote VARCHAR(128) NULL,
-  post_quote_author VARCHAR(128) NULL,
-  post_image VARCHAR(128) NULL,
-  post_video VARCHAR(128) NULL,
-  post_link VARCHAR(128) NULL,
-  post_count_views INT NOT NULL DEFAULT 0,
-  post_user_id INT NOT NULL,
-  post_content_type INT NOT NULL,
-  post_hashtag INT,
-  FOREIGN KEY(post_user_id) REFERENCES users(id),
-  FOREIGN KEY(post_content_type) REFERENCES content_types(id),
-  FOREIGN KEY(post_hashtag) REFERENCES hashtags -- ???
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title VARCHAR(128) NOT NULL,
+  text_content VARCHAR(255) NULL,
+  quote VARCHAR(128) NULL,
+  quote_author VARCHAR(128) NULL,
+  image VARCHAR(255) NULL,
+  video VARCHAR(255) NULL,
+  link VARCHAR(255) NULL,
+  count_views INT NOT NULL DEFAULT 0,
+  user_id INT NOT NULL,
+  content_type INT NOT NULL,
+  hashtag INT,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(content_type) REFERENCES content_types(id),
+  FOREIGN KEY(hashtag) REFERENCES hashtags(id)
 );
 
 CREATE TABLE IF NOT EXISTS comments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  comment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  comment_text_content VARCHAR(128) NOT NULL,
-  comment_author INT NOT NULL,
-  comment_post INT NOT NULL,
-  FOREIGN KEY(comment_author) REFERENCES users(id),
-  FOREIGN KEY(comment_post) REFERENCES posts(id)
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  text_content VARCHAR(128) NOT NULL,
+  author INT NOT NULL,
+  post INT NOT NULL,
+  FOREIGN KEY(author) REFERENCES users(id),
+  FOREIGN KEY(post) REFERENCES posts(id)
 );
 
 CREATE TABLE IF NOT EXISTS likes (
-  like_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  like_author INT NOT NULL,
-  like_post INT NOT NULL,
-  FOREIGN KEY(like_author) REFERENCES users(id),
-  FOREIGN KEY(like_post) REFERENCES posts(id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user INT NOT NULL,
+  post INT NOT NULL,
+  FOREIGN KEY(user) REFERENCES users(id),
+  FOREIGN KEY(post) REFERENCES posts(id)
 );
 
 CREATE TABLE IF NOT EXISTS subscriptions (
-  subscription_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  subscription_sender INT NOT NULL,
-  subscription_recipient INT NOT NULL,
-  FOREIGN KEY(subscription_sender) REFERENCES users(id),
-  FOREIGN KEY(subscription_recipient) REFERENCES users(id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sender INT NOT NULL,
+  recipient INT NOT NULL,
+  FOREIGN KEY(sender) REFERENCES users(id),
+  FOREIGN KEY(recipient) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS messages (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  message_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  message_text VARCHAR(128) NOT NULL,
-  message_sender INT NOT NULL,
-  message_recipient INT NOT NULL,
-  FOREIGN KEY(message_sender) REFERENCES users(id),
-  FOREIGN KEY(message_recipient) REFERENCES users(id)
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  text_content VARCHAR(128) NOT NULL,
+  sender INT NOT NULL,
+  recipient INT NOT NULL,
+  FOREIGN KEY(sender) REFERENCES users(id),
+  FOREIGN KEY(recipient) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS hashtags (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  hashtag_title VARCHAR(128) NOT NULL UNIQUE
+  title VARCHAR(128) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS content_types (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  content_type_title VARCHAR(128) NOT NULL UNIQUE,
-  content_type_icon VARCHAR(128) NOT NULL UNIQUE
+  title VARCHAR(128) NOT NULL UNIQUE,
+  icon VARCHAR(128) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS posts_hashtags (
+  post_id INT NOT NULL,
+  hashtag_id INT NOT NULL,
+  FOREIGN KEY(post_id) REFERENCES posts(id),
+  FOREIGN KEY(hashtag_id) REFERENCES hashtags(id)
 );
