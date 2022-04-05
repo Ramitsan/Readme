cbef350a5ec1fbf20a5a7fcc34c51db5d08bb420
Ramitsan <ramitsan@gmail.com>
2022-04-05 19:53:15 +0300

    Создает подключение к MySQL

--- settings.php
+++ settings.php
@@ -1,5 +1,9 @@
 <?php
 require_once('helpers.php');
 require_once('functions.php');
+require_once('db_functions.php');
 
 $quantity_hours_in_day = 24;
+
+// создаем переменную подключения, общую на весь проект
+$con = db_connect($db_config);
